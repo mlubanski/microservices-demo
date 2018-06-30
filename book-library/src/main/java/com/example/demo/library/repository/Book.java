765582bf9a4495bd64eade8824ce0690471d146f
Mateusz Lubański <m.lubanskii@gmail.com>
2018-06-30 22:30:05 +0200

    removing library from schema definition; defining entities for other tables

---
+++ book-library/src/main/java/com/example/demo/library/repository/Book.java
@@ -0,0 +1,28 @@
+package com.example.demo.library.repository;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+}
---
+++ book-library/src/main/java/com/example/demo/library/repository/BookLendingHistory.java
@@ -0,0 +1,80 @@
+package com.example.demo.library.repository;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Table(name="BOOK_LENDING_HISTORY")
+public class BookLendingHistory {
+
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+	
+	@ManyToOne(fetch=FetchType.EAGER)
+	private Book book;
+	
+	private String person;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="LENDING_DATE")
+	private Date lendingDate;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="RETURN_DATE")
+	private Date returnDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public String getPerson() {
+		return person;
+	}
+
+	public void setPerson(String person) {
+		this.person = person;
+	}
+
+	public Date getLendingDate() {
+		return lendingDate;
+	}
+
+	public void setLendingDate(Date lendingDate) {
+		this.lendingDate = lendingDate;
+	}
+
+	public Date getReturnDate() {
+		return returnDate;
+	}
+
+	public void setReturnDate(Date returnDate) {
+		this.returnDate = returnDate;
+	}
+
+	@Override
+	public String toString() {
+		return "BookLendingHistory [book=" + book + ", person=" + person + ", lendingDate=" + lendingDate
+				+ ", returnDate=" + returnDate + "]";
+	}
+}
